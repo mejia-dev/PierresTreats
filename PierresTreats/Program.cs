af43f2012f2e8e31bb7d98f6877e7257696d7833
mejia-dev <aaron.mejia8484@gmail.com>
2024-02-16 09:56:12 -0800

    Update context typo

--- PierresTreats/Program.cs
+++ PierresTreats/Program.cs
@@ -24,7 +24,7 @@ namespace PierresTreats
                       );
 
       builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ToDoListContext>()
+                .AddEntityFrameworkStores<PierresTreatsContext>()
                 .AddDefaultTokenProviders();
 
       WebApplication app = builder.Build();
